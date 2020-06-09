7149285e7437854ccb9e559dcf88ced9815bc30f
Eshan tripathi <eshan1285@gmail.com>
2020-06-09 10:24:34 +0530

    Updates

---
+++ precache-manifest.e559e873d0380f0d38ab39f68fa571bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bcfc760840c821b4a50ab7e4684ce3",
+    "url": "/Indic-shopping-store/index.html"
+  },
+  {
+    "revision": "9af2b4ac5377c75f5fbe",
+    "url": "/Indic-shopping-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "64d2ac11114193d6787b",
+    "url": "/Indic-shopping-store/static/css/main.a9ec377c.chunk.css"
+  },
+  {
+    "revision": "9af2b4ac5377c75f5fbe",
+    "url": "/Indic-shopping-store/static/js/2.d9d240e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Indic-shopping-store/static/js/2.d9d240e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d2ac11114193d6787b",
+    "url": "/Indic-shopping-store/static/js/main.76adc3b0.chunk.js"
+  },
+  {
+    "revision": "2d3a61a82c192a0d83cc",
+    "url": "/Indic-shopping-store/static/js/runtime-main.e966a97d.js"
+  }
+]);
