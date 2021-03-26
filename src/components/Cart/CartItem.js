fab7ff4a9b60877700adabdb2ff28fbfa8205eb1
eshanheretic <eshan1285@gmail.com>
2021-03-26 19:58:20 +0530

    package.json fix

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ({ item, value }) {
+export default function CartItem({ item, value }) {
   const { id, title, img, price, total, count } = item;
   const { increment, decrement, removeItem } = value;
   return (
@@ -24,19 +24,25 @@ export default function ({ item, value }) {
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
           <div>
-            <span className="btn btn-outline-dark mx-1" onClick={() => decrement(id)}>
+            <span
+              className="btn btn-outline-dark mx-1"
+              onClick={() => decrement(id)}
+            >
               -
             </span>
             <span className="btn btn-outline-dark mx-1">{count}</span>
-            <span className="btn btn-outline-dark mx-1" onClick={() => increment(id)}>
+            <span
+              className="btn btn-outline-dark mx-1"
+              onClick={() => increment(id)}
+            >
               +
             </span>
           </div>
         </div>
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <div className='cart-icon' onClick={()=> removeItem(id)}>
-            <i className='fas fa-trash'/>
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash" />
         </div>
       </div>
       <div className="col-10 mx-auto col-lg-2">
